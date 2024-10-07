f78e9182cba7c9d620aeba24b3a39af951524d92
danielsimonian <90415593+danielsimonian@users.noreply.github.com>
2024-10-07 11:55:51 -0300

    hospital

--- Sites/Hospital/script.js
+++ Sites/Hospital/script.js
@@ -3,3 +3,20 @@ $(document).ready(function(){
     $(".menu-bar").toggleClass( "open" );
     })
     })
+
+let nome = document.getElementById('nome')
+let idade = document.getElementById('idade')
+let altura = document.getElementById('altura')
+let peso = document.getElementById('peso')
+let imc = 0
+
+
+function calculaImc(){
+    let idadeN = Number(idade.value)
+    let alturaN = Number(altura.value)
+    let pesoN = Number(peso.value)
+    
+    imc = pesoN/(alturaN*alturaN)
+
+    console.log(imc)
+}
