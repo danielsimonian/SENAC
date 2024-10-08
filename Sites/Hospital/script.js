029a442dbe0c028c6b0818ccacbe6f52a1577e99
danielsimonian <90415593+danielsimonian@users.noreply.github.com>
2024-10-08 11:41:14 -0300

    db

--- Sites/Hospital/script.js
+++ Sites/Hospital/script.js
@@ -15,8 +15,17 @@ function calculaImc(){
     let idadeN = Number(idade.value)
     let alturaN = Number(altura.value)
     let pesoN = Number(peso.value)
-    
     imc = pesoN/(alturaN*alturaN)
-
     console.log(imc)
+
+    let melhorPlano = document.createElement('melhorPlano')
+    melhorPlano
 }
+
+
+/* let option = document.createElement("option")
+option.innerHTML = `Valor ${num.value} adicionado`
+lista.appendChild(option)
+console.log(valores)
+res.innerHTML = ""
+*/
