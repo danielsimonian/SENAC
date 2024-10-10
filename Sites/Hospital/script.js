ea9c4df6c74187cf71b577c157c9b3f6f12ab516
danielsimonian <90415593+danielsimonian@users.noreply.github.com>
2024-10-10 11:41:18 -0300

    site hospital

--- Sites/Hospital/script.js
+++ Sites/Hospital/script.js
@@ -11,6 +11,8 @@ let peso = document.getElementById('peso')
 let imc = 0
 
 
+
+
 function calculaImc(){
     let nome = document.getElementById('nome').value
     let idadeN = Number(idade.value)
@@ -18,40 +20,172 @@ function calculaImc(){
     let pesoN = Number(peso.value)
     let melhorPlano = document.getElementById('melhorPlano')
     imc = pesoN/(alturaN*alturaN)
-    console.log(imc)
+    let valorPlano = 399.99
 
-    if(idadeN >= 0 && idadeN < 5) {
-        melhorPlano.innerHTML =
-        `
-            <p>Plano Baby - Ideal para seu bebê!</p>
-        `
-    } else if(idadeN >= 5 && idadeN < 12) {
-        melhorPlano.innerHTML =
-        `
-            <div>
-            <p>Olá ${nome}, tudo bem?</p>
-            </div>
-            <p>Seu plano ideal é o Plano Criança - Ideal para sua criança!</p>
-        `
-    } else if(idadeN >= 12 && idadeN < 19) {
-        melhorPlano.innerHTML =
-        `
-            <p>Plano Adolescente - Ideal para seu adolescente!</p>
-        `
-    } else if(idadeN >= 19 && idadeN < 41) {
-        melhorPlano.innerHTML =
-        `
-            <p>Plano Adulto - Ideal para você que é adulto!</p>
-        `
-    } else if(idadeN >= 41 && idadeN < 61) {
-        melhorPlano.innerHTML =
-        `
-            <p>Plano Senior - Ideal para você que é senior!</p>
-        `
-    } else if(idadeN >= 61) {
-        melhorPlano.innerHTML =
-        `
-            <p>Plano Idoso - Ideal para você que é idoso!</p>
-        `
+    if(imc < 16 || imc > 30) {
+        valorPlano = valorPlano*1.2
     }
+        if (isNaN(nome) && idadeN, alturaN, pesoN != 0){
+            //bebe
+            if(idadeN >= 0 && idadeN < 5) {
+                valorPlano = (valorPlano*1.2).toFixed(2)
+                melhorPlano.innerHTML =
+                `
+                <div>
+                    <h3>Olá ${nome}, tudo bem?</h3>
+                </div>
+                <div>
+                    <p>Segundo o IMC do seu bebê de <strong>${imc}</strong></p>
+                </div>
+                <div>
+                    <h4>Seu plano ideal é o:</h4>
+                </div>
+                <div class="descricao-plano">
+                    <div>
+                        <h1>Plano Baby</h1>
+                    </div>
+                    <div>
+                        <p class="sub-titulo">Ideal para seu bebê!</p>
+                    </div>
+                    <div>
+                        <p class="preco">A partir de R$ ${valorPlano}</p>
+                    </div>
+                </div>
+                `
+                //crianca
+            } else if(idadeN >= 5 && idadeN < 12) {
+                valorPlano = (valorPlano*1.1).toFixed(2)
+                melhorPlano.innerHTML =
+                `
+                <div>
+                    <h3>Olá ${nome}, tudo bem?</h3>
+                </div>
+                <div>
+                    <p>Segundo o IMC do seu filho de <strong>${imc}</strong></p>
+                </div>
+                <div>
+                    <h4>Seu plano ideal é o:</h4>
+                </div>
+                <div class="descricao-plano">
+                    <div>
+                        <h1>Plano Kids</h1>
+                    </div>
+                    <div>
+                        <p class="sub-titulo">Ideal para seu filho!</p>
+                    </div>
+                    <div>
+                        <p class="preco">A partir de R$ ${valorPlano}</p>
+                    </div>
+                </div>
+                `
+                //adolescente
+            } else if(idadeN >= 12 && idadeN < 19) {
+                valorPlano = (valorPlano*1.15).toFixed(2)
+                melhorPlano.innerHTML =
+                `
+                <div>
+                    <h3>Olá ${nome}, tudo bem?</h3>
+                </div>
+                <div>
+                    <p>Segundo o IMC do seu filho de <strong>${imc}</strong></p>
+                </div>
+                <div>
+                    <h4>Seu plano ideal é o:</h4>
+                </div>
+                <div class="descricao-plano">
+                    <div>
+                        <h1>Plano Teens</h1>
+                    </div>
+                    <div>
+                        <p class="sub-titulo">Ideal para seu adolescente!</p>
+                    </div>
+                    <div>
+                        <p class="preco">A partir de R$ ${valorPlano}</p>
+                    </div>
+                </div>
+                `
+                //adulto
+            } else if(idadeN >= 19 && idadeN < 41) {
+                melhorPlano.innerHTML =
+                `
+                <div>
+                    <h3>Olá ${nome}, tudo bem?</h3>
+                </div>
+                <div>
+                    <p>Segundo seu IMC de <strong>${imc}</strong></p>
+                </div>
+                <div>
+                    <h4>Seu plano ideal é o:</h4>
+                </div>
+                <div class="descricao-plano">
+                    <div>
+                        <h1>Plano Adulto</h1>
+                    </div>
+                    <div>
+                        <p class="sub-titulo">Ideal para você!</p>
+                    </div>
+                    <div>
+                        <p class="preco">A partir de R$ ${valorPlano}</p>
+                    </div>
+                </div>
+                `
+                //senior
+            } else if(idadeN >= 41 && idadeN < 61) {
+                valorPlano = (valorPlano*1.2).toFixed(2)
+                melhorPlano.innerHTML =
+                `
+                <div>
+                    <h3>Olá ${nome}, tudo bem?</h3>
+                </div>
+                <div>
+                    <p>Segundo seu IMC de <strong>${imc}</strong></p>
+                </div>
+                <div>
+                    <h4>Seu plano ideal é o:</h4>
+                </div>
+                <div class="descricao-plano">
+                    <div>
+                        <h1>Plano Senior</h1>
+                    </div>
+                    <div>
+                        <p class="sub-titulo">Ideal para você!</p>
+                    </div>
+                    <div>
+                        <p class="preco">A partir de R$ ${valorPlano}</p>
+                    </div>
+                </div>
+                `
+                //idoso
+            } else if(idadeN >= 61) {
+                valorPlano = (valorPlano*1.4).toFixed(2)
+                melhorPlano.innerHTML =
+                `
+                <div>
+                    <h3>Olá ${nome}, tudo bem?</h3>
+                </div>
+                <div>
+                    <p>Segundo seu IMC de <strong>${imc}</strong></p>
+                </div>
+                <div>
+                    <h4>Seu plano ideal é o:</h4>
+                </div>
+                <div class="descricao-plano">
+                    <div>
+                        <h1>Plano Idoso</h1>
+                    </div>
+                    <div>
+                        <p class="sub-titulo">Ideal para você!</p>
+                    </div>
+                    <div>
+                        <p class="preco">A partir de R$ ${valorPlano}</p>
+                    </div>
+                </div>
+            `
+            }
+            
+            
+        } else {
+            alert('Favor digitar os campos vazios')
+        } 
+    
 }
